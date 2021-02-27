001081cb9109d94b19f5a26e6c5bba8b74a64a89
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2021-02-27 14:15:44 +0000

    Update lc0_policy_grapher.py

--- lc0_policy_grapher.py
+++ lc0_policy_grapher.py
@@ -163,11 +163,11 @@ def graph(nets, policies, values, title):
 	fig, ax = plt.subplots()
 	ax.plot(nets, policies, color = "red", marker = "o")
 	ax.set_xlabel("network", fontsize = 14)
-	ax.set_ylabel("policy (%)", color = "red", fontsize = 14)
+	ax.set_ylabel("policy", color = "red", fontsize = 14)
 
 	ax2 = ax.twinx()
 	ax2.plot(nets, values, color = "blue", marker = "o")
-	ax2.set_ylabel("value (%)", color = "blue", fontsize = 14)
+	ax2.set_ylabel("value", color = "blue", fontsize = 14)
 
 	plt.title(title)
 	plt.show()
@@ -197,7 +197,7 @@ def main():
 			policy, value = engine.test(fen, move, net)
 			nets.append(net)
 			policies.append(policy)
-			values.append(value * 100)
+			values.append(value)
 		except NetNotKnown:
 			print(f"(net {net} not known)")
 
